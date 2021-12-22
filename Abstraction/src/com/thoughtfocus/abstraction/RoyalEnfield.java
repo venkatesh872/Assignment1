0fe697b6e5e8011b6c6fec3c14d4a9398486eac8
venkatesh857 <venkateshofficial153@gmail.com>
2021-12-22 07:13:34 +0530

    abstraction code

---
+++ Abstraction/src/com/thoughtfocus/abstraction/RoyalEnfield.java
@@ -0,0 +1,14 @@
+package com.thoughtfocus.abstraction;
+
+public class RoyalEnfield extends Bike {
+	void breaktype() {
+		System.out.println("RoyalEnfield has Disk Break");
+		
+	}
+	void seattype() {
+		System.out.println("RoyalEnfield Has 1 seater");
+		
+	}
+	
+}
+
---
+++ Abstraction/src/com/thoughtfocus/abstraction/yamahaR15.java
@@ -0,0 +1,16 @@
+package com.thoughtfocus.abstraction;
+
+public class yamahaR15 extends Bike {
+		void breaktype() {
+			System.out.println("Yamaha R15RimBreak");
+			
+		}
+		void seattype() {
+			System.out.println("YamahaR15 has 2 seater");
+			
+		}
+		
+	}
+
+
+
