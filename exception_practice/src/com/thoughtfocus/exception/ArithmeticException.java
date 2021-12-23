45fb0acc741849078253c2cdee49333ee8368cb1
venkatesh857 <venkateshofficial153@gmail.com>
2021-12-23 07:58:32 +0530

    exception practice on arthemetic & outof bound

---
+++ exception_practice/src/com/thoughtfocus/exception/ArithmeticException.java
@@ -0,0 +1,10 @@
+package com.thoughtfocus.exception;
+
+public class ArithmeticException {
+	public static void main(String args[]) {
+		int a = 53;
+		int b = 0;
+		System.out.println(a/b);
+	}
+
+}
