06b0949c05c7dff2de880c85df9d430c162dbb99
venkatesh857 <venkateshofficial153@gmail.com>
2021-12-25 06:33:07 +0530

    type casting practice

---
+++ type_casting/src/com/thoughtFocus/Sports/TypeConversingTest.java
@@ -0,0 +1,31 @@
+package com.thoughtFocus.Sports;
+
+public class TypeConversingTest {
+	public static void main(String args[]) {
+		float number = 109;
+		int num = (int) number;
+		System.out.println(num);
+		
+		
+		short player = 1200;
+		double pl = player;
+		System.out.println(pl);
+		
+		byte price = 53;
+		int pr = price;
+		System.out.println(pr);
+		
+		
+		int number1 = 120;
+		byte num1 =(byte) number1;
+		System.out.println(num1);
+		
+		int number3 = 1200;
+		float num3 = (float) number3;
+		System.out.println(num3);
+		
+		
+		
+		
+	}
+}
